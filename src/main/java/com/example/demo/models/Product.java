c207aef7127652724df0dd4c4065618e297631bc
Mamonha <mamonha302@gmail.com>
2024-08-11 21:26:29 -0300

    Creating product model

---
+++ src/main/java/com/example/demo/models/Product.java
@@ -0,0 +1,24 @@
+package com.example.demo.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String descricao;
+    private Double preco;
+
+}
